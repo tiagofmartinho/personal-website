8c886f72d92f42dbda22584b60ad7b01234c997e
Tiago Martinho <tfmss1@iscte-iul.pt>
2019-10-27 20:47:47 +0000

    Changed website title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Tiago Martinho | Software Engineer';
 }
