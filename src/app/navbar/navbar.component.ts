a6d6e25809438fe0783b0ebb71e2badc1ee76b7d
Tiago Martinho <tfmss1@iscte-iul.pt>
2021-04-13 22:11:04 +0100

    update angular

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,34 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  name = 'Tiago Martinho';
-  navBarLinks: string[] = [
-    'About', 'Skills', 'Resume', 'Contact'
-  ];
-  activeNavLinkId = '';
+  name = "Tiago Martinho";
+  navBarLinks: string[] = ["About", "Skills", "Resume", "Contact"];
+  activeNavLinkId = "";
 
   setActive(id) {
     // if a new nav bar link is clicked, disable the old one and set the new one
     if (id !== this.activeNavLinkId) {
-      if (this.activeNavLinkId !== '') {
-        document.getElementById(this.activeNavLinkId).className = 'nav-link';
+      if (this.activeNavLinkId !== "") {
+        document.getElementById(this.activeNavLinkId).className = "nav-link";
       }
-      if (id !== '') {
-        document.getElementById(id).className = 'nav-link active';
+      if (id !== "") {
+        document.getElementById(id).className = "nav-link active";
       }
       this.activeNavLinkId = id;
     }
   }
-
 }
